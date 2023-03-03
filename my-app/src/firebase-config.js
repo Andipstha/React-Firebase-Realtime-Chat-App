f6bc956a4b2201c09aa9750c574a77ad74e9df45
andipstha <sandip.stha120@gmail.com>
2023-03-04 01:12:37 +0545

    Installed Firebase and universal-cookie package | config Firebase #2

---
+++ my-app/src/firebase-config.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyARjBnHRz38kC381h4bcuq2wPHHz4tXTFA",
+  authDomain: "chatapp-22ef8.firebaseapp.com",
+  projectId: "chatapp-22ef8",
+  storageBucket: "chatapp-22ef8.appspot.com",
+  messagingSenderId: "71116688964",
+  appId: "1:71116688964:web:48424aa1230406c946b611",
+  measurementId: "G-5SQCXWR21B"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
