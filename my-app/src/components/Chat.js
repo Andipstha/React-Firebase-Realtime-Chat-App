ee966a7f4531be1ef709e28c6f9df6bb8e80cd91
andipstha <sandip.stha120@gmail.com>
2023-03-04 16:15:19 +0545

    Update Chat Components #3.1

--- my-app/src/components/Chat.js
+++ my-app/src/components/Chat.js
@@ -1,15 +1,30 @@
-import { useState } from "react";
-import { addDoc, collection, serverTimestamp } from "firebase/firestore";
+import { useEffect, useState } from "react";
+import { addDoc, collection, serverTimestamp, onSnapshot, query, where } from "firebase/firestore";
 // import { async } from "@firebase/util";
 import {auth, db} from  "../firebase-config"
 import "../styles/Chat.css";
+
+
 export const Chat = (props) => {
 
-    const {room} = props;
-    
+    const { room } = props;
     const [newMessage, setNewMessage] = useState("");
+    const [messages, setMessages] = useState([]);
 
     const messagesRef = collection(db, "messages");
+
+    useEffect(() => {
+        const queryMessages = query(messagesRef, where("room", "==", room));
+        const unsuscribe = onSnapshot(queryMessages, (snapshot) => { 
+            let messages = [];
+            snapshot.forEach((doc) =>{
+                messages.push({ ...doc.data(), id: doc.id });
+            });
+            setMessages(messages);
+        });
+
+        return() => unsuscribe();
+    }, []);
 
     const handleSubmit = async (e) => {
 
@@ -29,11 +44,16 @@ export const Chat = (props) => {
 
     return (
         <div className = "chat-app">
+            <div className="header">
+                <h1>Welcomt to: {room.toUpperCase()}</h1>
+            </div>
+            <div className="messages"> 
+                {messages.map((message) => <h1> {message.text} </h1>)} 
+            </div>
             <form onSubmit={handleSubmit} className="new-message-from">
                 <input 
                     className="new-message-input" 
-                    placeholder="Type your message here.. "
-                    onChange={(e) => setNewMessage(e.target.value)} 
+                    placeholder="Type your message here.. "                        onChange={(e) => setNewMessage(e.target.value)} 
                     value={newMessage}
                 />
                 <button type="submit" className="send-button">
